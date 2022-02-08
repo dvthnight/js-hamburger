90d58b1b1addd9fb55277cfa55b4a960dd99a6e4
dvthnight <davide.lanotte@protonmail.com>
2022-02-08 16:34:42 +0100

    esercizio ultimato con la funzione close

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // creo una classe nell'ancora che contiene il pulsante a forma di hamburger
 const hamburgerButton = document.querySelector(".header-right > a");
-hamburgerButton.classList.add("button");
+hamburgerButton.classList.add("hamburger-button");
 console.log(hamburgerButton);
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+console.log(hamburgerMenu);
 
 
 // abilito la funzione per cliccare sul pulsante
 hamburgerButton.addEventListener("click", function(){
+
     // al click si attiva la classe active alll'interno dell'hamburger-menu
-    const hamburgerMenu = document.querySelector(".hamburger-menu");
     hamburgerMenu.classList.add("active");
-    console.log(hamburgerMenu);
+})
+
+const closeButton = document.querySelector(".close");
+
+// al click della X si disabilita la classe active
+
+closeButton.addEventListener("click", function(){
+    hamburgerMenu.classList.remove("active");
 })
